9d12a3e3d1c794defcb1a110fd8af0df3697fe20
eugen <eugen.yzeiri@gmail.com>
2019-03-02 16:08:07 +0100

    upgrade to Gatsby 2.0

---
+++ styles-5efd947a2dc41e31fce1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{188:function(n,o,w){},189:function(n,o,w){},192:function(n,o,w){}}]);
+//# sourceMappingURL=styles-5efd947a2dc41e31fce1.js.map
