e58a925b16e59325bb14efb03c50c51da47d80e1
eugen <eugen.yzeiri@gmail.com>
2019-03-03 15:02:37 +0100

    push new favicons

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5a64ebc0d8f15cb4855e.js"
+    "url": "webpack-runtime-409b0ac03a66e8972fae.js"
   },
   {
     "url": "app-f0a8a6800f541793e262.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "328ecd525c594dd374998f9246709ba6"
+    "revision": "1c12f09c971b5264f6ed65de0cad071a"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
